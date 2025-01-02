440d57d50069e0c732a4e8e64c5b22fedaefec60
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2025-01-01 23:26:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//Prototype-based OOP: In JavaScript, objects are created from other objects using prototypes. Think of it as inheritance without classes: objects can directly inherit behavior and properties from other objects (prototypes). For example:
+
+const car = { color: "red", make: "Acura"}
+const myCar = Object.create(car);
+console.log(`the car is ${myCar.color}`)
+
+const house = { rooms: 4, kitchen: 1, livingRoom: 3, garden: true}
+const myHouse = Object.create(house);
+console.log(`the house has ${myHouse.rooms} and ${myHouse.livingRoom} living Room`)
+
